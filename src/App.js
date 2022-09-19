24ad1056f2f46351e9a2ac3f6cec4d4317abfd8f
nazero3 <nazerrabah1994@gmail.com>
2022-09-19 14:37:56 +0300

    check the comment for the pop up window

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.css';
 import UserForm from './components/UserForm';
 import {useState} from 'react';
 import Users from './components/Users';
+import './components/UsersList.css'
 
 function App() {
-  
+  let allTrue = false;
   const [usersList, setUsersList] = useState('');
   const onSaveNewUser = (user) => {
     setUsersList((item) => {
       return[user,...item];
     })
   }
+  //https://minutemailer.github.io/react-popup/
   return (
     <div>
       <UserForm onSaveList={onSaveNewUser}/>
       
       <Users items={usersList}/>
+      {!allTrue && (<span className="model">lalala</span>)}
     </div>
   );
 }
